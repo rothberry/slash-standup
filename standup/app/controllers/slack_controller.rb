7201183ac9ff88aa3d059d124ea2905a7709ec46
phil <phil.roth077@gmail.com>
2025-07-10 16:49:19 -0400

    connected, working through interactions

--- standup/app/controllers/slack_controller.rb
+++ standup/app/controllers/slack_controller.rb
@@ -1,17 +1,43 @@
 require 'pry'
+require 'json'
+
 class SlackController < ApplicationController
 
   def slashcommand
     puts "SLASHCOMMAND"
     puts params
-    binding.pry
-    
+    file = File.open("./app/controllers/modal.json")
+    data = JSON.load(file)
+    puts data
+    render json: data
   end
   
   def interactivity
     puts "INTERACT"
     puts params
-    binding.pry
+
+    payload = JSON.parse(params[:payload])
+    channel_id = payload["channel"]["id"]
+    
+    action = payload["actions"][0]["action_id"]
+
+    # if action is submit, try to save to database
+    # if action is cancel, close message/modal
+    # err..
+    if action == "submit-action"
+      puts "SUBMITTING"
+      state = payload["state"]["values"]
+      question_hash = {}
+      state.each do |_, act|
+        k, v = act.keys[0], act[act.keys[0]]["value"]
+        question_hash[k] = v
+      end
+      render json: {}
+    elsif action == "cancel-action"
+      puts "CANCELING"
+      render json: {
+        text: 'BYEEEEE'}
+    end
     
   end
 
